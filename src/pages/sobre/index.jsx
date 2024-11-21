2e98de71f37ab2e98e400b0b93d194200bbe7801
gabbiee33 <gabriellepereirarocha96@gmail.com>
2024-11-21 00:22:50 -0300

    finalização pag sobre

--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -45,6 +45,7 @@ export default function Sobre() {
           <div className='apresentaçao'>
 
             <h3>Gabrielle Pereira</h3>
+            <br />
             <h3>Web Designer e Desenvolvedora Front-end</h3>
 
             <p>Olá, prazer em te conhecer. Sou Gabrielle Pereira mas você pode me chamar de Gabi. Tenho 17 anos e sou extremamente aberta a novas experiências. Minhas expectativas para a nossa empresa é que ela cresça e atinja diferentes públicos e realidades, que leve todo nosso trabalho e criatividade para as mais diferentes pessoas e que possamos ser reconhecidos por isso.
@@ -53,7 +54,7 @@ export default function Sobre() {
 
           </div>
 
-          <img src="/assets/images/defaultwebber.jpg" alt="foto de Gabrielle Pereira" className='foto-webber' />
+          <img src="/assets/images/gabi2.jpg" alt="foto de Gabrielle Pereira" className='foto-webber' />
 
         </section>
 
@@ -62,13 +63,14 @@ export default function Sobre() {
           <div className='apresentaçao'>
 
             <h3>Felipe Costa</h3>
+            <br />
             <h3>Desenvolvedor Fullstack</h3>
 
             <p>Olá! Eu me chamo Felipe Costa e sou um dos desenvolvedores integrantes do grupo Webbo. Com a iniciativa da nossa empresa, eu busco levar a alegria de ver diversas portas se abrindo, graças a um site, para as outras pessoas. E como um dos Webbers, eu estou disposto a entregar o meu melhor para que a experiência de nossos clientes, que eu garanto que vão ver seus projetos alavancarem. Não sou alguém que desiste fácil, mas sou uma pessoa que gosta de resolver erros, então mesmo que tudo pareça dar errado, eu estarei pronto para entender, analisar, e resolver todos os problemas que pudermos enfrentar na nossa trajetória e de seu site.</p>
 
           </div>
 
-          <img src="/assets/images/defaultwebber.jpg" alt="foto de Felipe Costa" className='foto-webber' />
+          <img src="/assets/images/fefo2.jpg" alt="foto de Felipe Costa" className='foto-webber' />
 
         </section>
 
@@ -77,6 +79,7 @@ export default function Sobre() {
           <div className='apresentaçao'>
 
             <h3>Letícia Koga</h3>
+            <br />
             <h3>Web Designer e Desenvolvedor Front-end</h3>
 
             <p>Olá, me chamo Le e sou um dos desenvolvedores da Webbo, atuando como web designer e desenvolvedor Front-end. Meu trabalho é transformar conceitos em experiências digitais dinâmicas e atraentes. Utilizo minha experiência para criar interfaces intuitivas e funcionais que destacam a estética e a usabilidade.
@@ -85,7 +88,7 @@ export default function Sobre() {
 
           </div>
 
-          <img src="/assets/images/defaultwebber.jpg" alt="foto de Leticia Koga" className='foto-webber' />
+          <img src="/assets/images/lele.jpg" alt="foto de Leticia Koga" className='foto-webber' />
 
         </section>
 
